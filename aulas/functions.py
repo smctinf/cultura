a93655152829c8cc1d02192aa5ff9a1d4c639af1
Musashi <80537456+salarini-e@users.noreply.github.com>
2024-04-05 14:08:08 -0300

    Update functions.py

--- aulas/functions.py
+++ aulas/functions.py
@@ -11,7 +11,7 @@ import re
 
 from django.core.validators import EMPTY_VALUES
 from django.forms import ValidationError
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 error_messages = {
     'invalid': _("Número de CPF inválido."),
